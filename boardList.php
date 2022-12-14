d9c9d5bf08daed9d4a2c0b8cfe94d979bd19698b
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2022-12-14 14:46:33 +0100

    added content to boardList.php
    though there is no way to create a board yet

--- boardList.php
+++ boardList.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+    $conn = DbConnect();
+    $boards = fetchFromDb($conn, "boards", "userId", $_SESSION["username"]);
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +10,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+    <link rel="stylesheet" href="style.css"> 
+    <title>Board list</title>
 </head>
 <body>
-    
+    <?php
+        if(count($boards) != 0){
+            foreach($boards as $board){
+                $boardData = json_decode($board["contents"]);?>
+                <div class="boardListContainer">
+                    <h1><?php echo $boardData["title"];?></h1>
+                    <p><?php echo $boardData["description"];?></p>
+                </div>
+            <?php}
+        }
+    ?>
 </body>
 </html>
